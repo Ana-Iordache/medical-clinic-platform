206ce0cc206d0286f04c64be98b924cc44163240
Iordache Ana-Maria <anaiordache264@gmail.com>
2024-05-11 18:07:38 +0300

    added mongodb connection

--- server/index.js
+++ server/index.js
@@ -5,6 +5,27 @@ const config = require('../config.json');
 const port = config.BACKEND_PORT;
 const routes = require('./router/routes');
 const cors = require('cors');
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+const dbURI = `mongodb://${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`;
+const dbConnection = mongoose.connection;
+
+dbConnection.on("disconnected", () => {
+  setTimeout(() => {
+    connectToMongoDB(dbURI);
+  }, 5000);
+});
+
+dbConnection.on("reconnected", () => {
+  console.log(`DB reconnected to ${dbURI}`);
+});
+
+dbConnection.once("open", () => {
+  console.log(`Connected successfully to DB ${dbURI}`);
+});
+
+connectToMongoDB(dbURI);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
@@ -16,4 +37,11 @@ app.use(cors());
 // Middleware to parse JSON in the request body
 app.use(bodyParser.json());
 
-app.use('/', routes)
+app.use(config.BASE_URI, routes)
+
+function connectToMongoDB(uri) {
+  mongoose.connect(uri)
+  .catch(err => {
+    console.log(`Failed to connect to DB ${uri}: ${err}`)
+  })
+}
