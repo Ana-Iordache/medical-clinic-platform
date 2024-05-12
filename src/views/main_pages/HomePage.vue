83218f3d17960927597b51c91ddb77a87fd8ef38
Iordache Ana-Maria <anaiordache264@gmail.com>
2024-05-12 18:57:42 +0300

    import authentication store in home page

--- src/views/main_pages/HomePage.vue
+++ src/views/main_pages/HomePage.vue
@@ -1,9 +1,25 @@
 <template>
-    Home page
+    <div v-if="authenticationStore.user">
+        Hello, {{ currentUserFullName }}
+    </div>
 </template>
 
 <script>
+import { mapStores } from 'pinia';
+import { useAuthenticationStore } from '../../pinia_stores/authenticationStore';
+
 export default {
     name: 'HomePage',
+
+    data: () => ({
+        
+    }),
+    
+    computed: {
+        ...mapStores(useAuthenticationStore),
+        currentUserFullName() {
+            return `${this.authenticationStore.user.firstName}  ${this.authenticationStore.user.lastName}`;
+        }
+    }
 }
 </script>
