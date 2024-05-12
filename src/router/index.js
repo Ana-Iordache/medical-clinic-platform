85feb2540a06a65a397077d54a5080b6a20257d4
Iordache Ana-Maria <anaiordache264@gmail.com>
2024-05-12 18:49:51 +0300

    restict FE routes for unauthenticated users

--- src/router/index.js
+++ src/router/index.js
@@ -12,29 +12,125 @@ import RateApplication from '@/views/account_section/RateApplication'
 import Login from '@/views/authentication_forms/Login'
 import SignUp from '@/views/authentication_forms/SignUp'
 import ResetPassword from '@/views/authentication_forms/ResetPassword'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const routes = [
-    { path: '/', redirect: '/home' },
-    { path: '/logout', redirect: '/home' },
-    { path: '/home', component: HomePage },
-    { path: '/patients', component: UsersList },
-    { path: '/doctors', component: UsersList },
-    { path: '/appointments', component: Appointments },
-    { path: '/feedback', component: Feedback },
-    { path: '/earnings', component: Earnings },
-    { path: '/services', component: Services },
-    { path: '/payments', component: Payments },
-    { path: '/profile', component: Profile },
-    { path: '/settings', component: Settings },
-    { path: '/rate_application', component: RateApplication },
-    { path: '/login', component: Login },
-    { path: '/sign_up', component: SignUp },
-    { path: '/reset_password', component: ResetPassword },
+    {
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/logout',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        component: HomePage
+    },
+    {
+        path: '/patients',
+        component: UsersList,
+        meta: {
+            requiredAuth: true
+        }
+    },
+    {
+        path: '/doctors',
+        component: UsersList
+    },
+    {
+        path: '/appointments',
+        component: Appointments,
+        meta: {
+            requiredAuth: true
+        }
+    },
+    {
+        path: '/feedback',
+        component: Feedback,
+        meta: {
+            requiredAuth: true
+        }
+    },
+    {
+        path: '/earnings',
+        component: Earnings,
+        meta: {
+            requiredAuth: true
+        }
+    },
+    {
+        path: '/services',
+        component: Services
+    },
+    {
+        path: '/payments',
+        component: Payments,
+        meta: {
+            requiredAuth: true
+        }
+    },
+    {
+        path: '/profile',
+        component: Profile,
+        meta: {
+            requiredAuth: true
+        }
+    },
+    {
+        path: '/settings',
+        component: Settings,
+        meta: {
+            requiredAuth: true
+        }
+    },
+    {
+        path: '/rate_application',
+        component: RateApplication,
+        meta: {
+            requiredAuth: true
+        }
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/sign_up',
+        component: SignUp
+    },
+    {
+        path: '/reset_password',
+        component: ResetPassword
+    },
 ]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
 });
+
+// without this, when we hit refresh, even if we are logged in because by the time the refresh page happens
+// firebase current user doesn't exists (firebase isn't initialized yet)
+const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const removeListener = onAuthStateChanged(getAuth(), user => {
+            removeListener();
+            resolve(user);
+        }, reject);
+    })
+}
+
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some(record => record.meta.requiredAuth)) {
+        if (await getCurrentUser()) {
+            next();
+        } else {
+            next("/login");
+        }
+    } else {
+        next();
+    }
+})
 
 export default router;
