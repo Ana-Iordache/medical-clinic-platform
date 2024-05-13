4921add367b78ce44f35e09bfc431306c42d0b1c
Iordache Ana-Maria <anaiordache264@gmail.com>
2024-05-13 15:14:24 +0300

    display users list in page wip

--- src/views/main_pages/UsersList.vue
+++ src/views/main_pages/UsersList.vue
@@ -2,7 +2,85 @@
     <div ref="users_list_container" id="users_list_container" :style="containerStyle" class="page_container">
         <div class="text-h4 page_title"> {{ pageTitle }} </div>
         <div class="page_content_overlay">
+            <v-data-iterator :items="usersList" :items-per-page="6" :search="search">
+                <template v-slot:header>
+                    <v-toolbar class="px-2" color="transparent">
+                        <v-text-field
+                            v-model="search"
+                            density="comfortable"
+                            placeholder="Search"
+                            prepend-inner-icon="mdi-magnify"
+                            style="max-width: 300px"
+                            variant="solo"
+                            clearable
+                            hide-details
+                        ></v-text-field>
+                    </v-toolbar>
+                </template>
 
+                <template v-slot:default="{ items }">
+                    <v-container class="pa-2" fluid>
+                        <v-row dense>
+                            <v-col v-for="item in items" :key="item._id" cols="auto" md="4">
+                                <v-card class="pb-3" border flat>
+                                    <div class="text-h6 text-center"> {{ item.raw.firstName }} {{ item.raw.lastName }}</div>
+
+                                    <div class="user_info_area">
+                                        <!-- TODO: check how it is displayed with an actual photo -->
+                                        <v-avatar :image="getProfilePhotoUrl(item.raw)" size="100"></v-avatar>
+                                        <v-list lines="one">
+                                            <div v-if="item.raw.role == 'doctor'">
+                                                <v-list-item
+                                                    v-for="info in doctorUserInfo"
+                                                    :key="info"
+                                                    :title="info.title"
+                                                    :subtitle="item.raw[info.key] ?? '-'"
+                                                ></v-list-item>
+                                            </div>
+
+                                            <div v-else-if="item.raw.role == 'patient'">
+                                                <v-list-item
+                                                    v-for="info in patientUserInfo"
+                                                    :key="info"
+                                                    :title="info.title"
+                                                    :subtitle="item.raw[info.key] ?? '-'"
+                                                ></v-list-item>
+                                            </div>
+                                        </v-list>
+                                    </div>
+
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </template>
+
+                <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
+                    <div class="d-flex align-center justify-center pa-4">
+                    <v-btn
+                        :disabled="page === 1"
+                        density="comfortable"
+                        icon="mdi-arrow-left"
+                        variant="tonal"
+                        rounded
+                        @click="prevPage"
+                    ></v-btn>
+
+                    <div class="mx-2 text-caption">
+                        Page {{ page }} of {{ pageCount }}
+                    </div>
+
+                    <v-btn
+                        :disabled="page >= pageCount"
+                        density="comfortable"
+                        icon="mdi-arrow-right"
+                        variant="tonal"
+                        rounded
+                        @click="nextPage"
+                    ></v-btn>
+                    </div>
+                </template>
+            </v-data-iterator>
         </div>
     </div>
 </template>
@@ -15,7 +93,18 @@ export default {
     name: 'UsersList',
 
     data: () => ({
-        usersList: []
+        usersList: [],
+        search: "",
+        doctorUserInfo: [
+            { title: "Professional degree", key: "professionalDegree" },
+            { title: "Specialization",      key: "specialization" },
+            { title: "Phone",               key: "phoneNumber" }
+        ],
+        patientUserInfo: [
+            { title: "CNP",              key: "identityNumber" },
+            { title: "Last appointment", key: "lastAppointment" }, // TODO: get last appointment in BE
+            { title: "Phone",            key: "phoneNumber" }
+        ]
     }),
 
     computed: {
@@ -46,7 +135,8 @@ export default {
             return {
                 backgroundImage: `url(${this.backgroundImageUrl})`,
                 backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover'
+                backgroundSize: 'cover',
+                height: '100vh'
             }
         },
         getUsersUrl() {
@@ -61,12 +151,15 @@ export default {
     },
 
     async mounted() {
+        // TODO: when page is refreshed, the pinia store is loaded after mounted. FIX THIS
        await this.loadUsers();
     },
 
     methods: {
         loadUsers() {
             const url = this.getUsersUrl;
+            console.log("DEBUG currentUserRole: ", this.currentUserRole)
+            console.log("DEBUG url: ", url)
             return new Promise(resolve => {
                 this.axios.get(url)
                     .then(response => response.data)
@@ -74,10 +167,19 @@ export default {
                     .catch(error => console.error(error))
                     .finally(() => resolve());
             })
+        },
+        getProfilePhotoUrl(user) {
+           return user.profilePhotoUrl ? user.profilePhotoUrl : require('@/assets/user_profile.jpg');
         }
     }
 }
 </script>
 
 <style scoped>
+.user_info_area {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+}
 </style>
