c3e546f22b2f2026fd2082aa973b539baa8b6ab6
Iordache Ana-Maria <anaiordache264@gmail.com>
2024-05-15 18:08:58 +0300

    update controller to get appointments by date

--- server/commons/aggregations/appointments.js
+++ server/commons/aggregations/appointments.js
@@ -35,36 +35,81 @@ async function getAppointmentsForPatient(email) {
     ])
 }
 
-async function getAppointmentsForDoctor(email) {
-    return await Appointments.aggregate([
-        {
-          $match: {
-            doctorEmail: email
-          }
-        },
-        {
-          $lookup: {
-            from: "users",
-            localField: "patientEmail",
-            foreignField: "email",
-            as: "patientDetails"
-          }
-        },
-        {
-          $unwind: "$patientDetails"
-        },
-        {
-          $project: {
-            _id: 1,
-            dateAndTime: 1,
-            status: 1,
-            prescriptionUrl: 1,
-            patientFullName: {
-                $concat: [ "$patientDetails.firstName", " ", "$patientDetails.lastName" ]
+async function getAppointmentsForDoctor(email, date) {
+  const mainStages = [
+    {
+      $match: {
+        doctorEmail: email
+      }
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "patientEmail",
+        foreignField: "email",
+        as: "patientDetails"
+      }
+    },
+    {
+      $unwind: "$patientDetails"
+    }
+  ]
+
+  let stages;
+
+  // if we get the date as a parameter, we need the appointments from that spefici date that are not canceled
+  // in order to set available hours for a new appointment
+  if(date) {
+    stages = [
+      ...mainStages,
+      {
+        $addFields: {
+          dateAsString: {
+            $dateToString: {
+              format: "%m/%d/%Y",
+              date: "$dateAndTime"
+            }
+          },
+          hourAsString: {
+            $dateToString: {
+              format: "%H:%M",
+              date: "$dateAndTime"
             }
           }
         }
-    ])
+      },
+      {
+        $match: {
+          dateAsString: date,
+          status: { $ne: "canceled" }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          date: "$dateAsString",
+          hour: "$hourAsString"
+        }
+      }
+    ]
+  } else {
+    stages = [
+      ...mainStages,
+      {
+        $project: {
+          _id: 1,
+          dateAndTime: 1,
+          status: 1,
+          prescriptionUrl: 1,
+          patientFullName: {
+            $concat: [ "$patientDetails.firstName", " ", "$patientDetails.lastName" ]
+          }
+        }
+      }
+    ]
+  }
+
+  return await Appointments.aggregate(stages);
 }
 
 module.exports = {
--- server/controllers/appointments.js
+++ server/controllers/appointments.js
@@ -1,15 +1,17 @@
 const { getAppointmentsForPatient, getAppointmentsForDoctor } = require("../commons/aggregations/appointments");
 
 // GET /users/:userEmail/appointments?role=[doctor, patient]
+// TODO: maybe I should get the role directly from Users collection
 async function getMany(req, res) {
     const role = req.query.role;
+    const date = req.query.date;
     const userEmail = req.params.userEmail;
 
     let appointments;
     if(role == "patient") {
         appointments = await getAppointmentsForPatient(userEmail);
     } else {
-        appointments = await getAppointmentsForDoctor(userEmail);
+        appointments = await getAppointmentsForDoctor(userEmail, date);
     }
 
     if (appointments.length > 0) {
