1e3f1a3630362067e2eba2bc451102b05d0842f5
Iordache Ana-Maria <anaiordache264@gmail.com>
2024-05-16 23:51:18 +0300

    small update on requiredField rule

--- src/commons/mixins.js
+++ src/commons/mixins.js
@@ -1,7 +1,7 @@
 const generalMixin = {
     data: () => ({
         rules: {
-            requiredField: value => !!value || "This field is required.",
+            requiredField: value => !!value.trim() || "This field is required.",
             tenCharacters: value => value.length == 10 || "This field should be 10 charcaters long.",
             thirthteenCharacters: value => value.length == 13 || "This field should be 13 charcaters long.",
             password: value => (value.length >= 6 && value.search(/[a-z]/) >= 0 && value.search(/[A-Z]/) >= 0 && value.search(/[0-9]/) >= 0) || "Password is not valid."
