0130d017fc9ae02a5037ef323f25ed92cf860535
Iordache Ana-Maria <anaiordache264@gmail.com>
2024-05-16 23:52:03 +0300

    created loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-list class="py-2" color="primary" elevation="12" rounded="lg">
+        <v-list-item prepend-icon="mdi-heart-pulse" :title="`${title}...`">
+            <template v-slot:prepend>
+                <div class="pe-4">
+                    <v-icon color="primary" size="x-large"></v-icon>
+                </div>
+            </template>
+
+            <template v-slot:append>
+                <v-progress-circular color="primary" indeterminate="disable-shrink" size="26"
+                    width="3"></v-progress-circular>
+            </template>
+        </v-list-item>
+    </v-list>
+</template>
+
+<script>
+export default {
+    name: "LoadingComponent",
+    props: {
+        title: {
+            type: String,
+            default: "Loading data"
+        }
+    }
+}
+</script>
--- src/views/authentication_forms/SignUp.vue
+++ src/views/authentication_forms/SignUp.vue
@@ -142,28 +142,20 @@
     </div>
 
     <v-dialog v-model="showLoadingDialog" max-width="320" persistent>
-        <v-list class="py-2" color="primary" elevation="12" rounded="lg">
-            <v-list-item prepend-icon="mdi-heart-pulse" title="Creating account...">
-                <template v-slot:prepend>
-                    <div class="pe-4">
-                        <v-icon color="primary" size="x-large"></v-icon>
-                    </div>
-                </template>
-
-                <template v-slot:append>
-                    <v-progress-circular color="primary" indeterminate="disable-shrink" size="26" width="3"></v-progress-circular>
-                </template>
-            </v-list-item>
-        </v-list>
+        <Loading :title="'Creating account'"></Loading>
     </v-dialog>
 </template>
 
 <script>
 import generalMixin from "@/commons/mixins";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import Loading from '@/components/Loading.vue';
 
 export default {
     name: 'SignUpForm',
+    components: {
+        Loading
+    },
     mixins: [generalMixin],
     data: () => ({
         userData: {
