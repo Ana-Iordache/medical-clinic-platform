d6d7f3c27ed6c09c2eeac94027625fd346bf8bb3
Iordache Ana-Maria <anaiordache264@gmail.com>
2024-05-17 12:33:04 +0300

    configurable port for frontend

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+const config = require('./config.json');
 module.exports = defineConfig({
   transpileDependencies: true,
 
@@ -6,5 +7,9 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
+  },
+
+  devServer: {
+    port: config.FRONTEND_PORT
   }
 })
